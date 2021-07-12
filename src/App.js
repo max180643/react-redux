f6484aed3ab7ae51efe44e90c5a333531f6999d0
max180643 <max180643@gmail.com>
2021-07-12 18:40:11 +0700

    :rocket: use redux thunk

--- src/App.js
+++ src/App.js
@@ -1,29 +1,47 @@
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-import Nav from './components/Nav'
-import Products from './pages/Products'
-import Signin from './pages/SIgnin'
-import Cart from './pages/Cart'
+import Nav from './components/Nav';
+import Products from './pages/Products';
+import Signin from './pages/SIgnin';
+import Cart from './pages/Cart';
 
-import './App.css'
+import './App.css';
 
-function App() {
+function UnAuthApp() {
   return (
-    <div className='App'>
-      <Nav />
-      <Switch>
-        <Route path='/signin'>
-          <Signin />
-        </Route>
-        <Route path='/cart'>
-          <Cart />
-        </Route>
-        <Route path='/'>
-          <Products />
-        </Route>
-      </Switch>
-    </div>
-  )
+    <Switch>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/">
+        <Products />
+      </Route>
+    </Switch>
+  );
 }
 
-export default App
+function AuthApp() {
+  return (
+    <Switch>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+      <Route path="/">
+        <Products />
+      </Route>
+    </Switch>
+  );
+}
+
+function App() {
+  const { user } = useSelector((state) => state.auth);
+  return (
+    <div className="App">
+      <Nav />
+      {!user ? <UnAuthApp /> : <AuthApp />}
+    </div>
+  );
+}
+
+export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {useSelector} from "react-redux"
+import {useSelector, useDispatch} from 'react-redux'
+
+import {setAuth} from '../actions/authActions'
 
 export default function Nav() {
   const cart = useSelector((state) => state.cart)
+  const { user } = useSelector(state => state.auth)
+  
+  const dispatch = useDispatch()
 
   return (
     <header className='head'>
@@ -12,13 +17,15 @@ export default function Nav() {
           <li className='nav-list'>
             <Link to='/'>Products</Link>
           </li>
+          {user &&
+            <li className='nav-list'>
+              <Link to='/cart'>Cart <span className='cart-num'>
+                {cart.reduce((sum, item) => sum + item.quantity, 0)}
+              </span></Link>
+            </li>
+          }
           <li className='nav-list'>
-            <Link to='/cart'>Cart <span className='cart-num'>
-              {cart.reduce((sum, item) => sum + item.quantity, 0)}
-            </span></Link>
-          </li>
-          <li className='nav-list'>
-            <Link to='/signin'>Sign in</Link>
+            {user ? <button onClick={() => dispatch(setAuth(null))}>Sign out</button> : <Link to='/signin'>Sign in</Link>}
           </li>
         </ul>
       </div>
